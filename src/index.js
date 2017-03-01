85c3c714193167fa1effd24206cfb41c916021fe
Uslu, Sureyya <Sureyya.Uslu@ca.com>
2017-03-02 00:23:59 +0100

    Add testing and webpack config

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
+import './index.css';
+
 import numeral from 'numeral';
+
+//disable eslint in this file
+/* eslint-disable no-console */
+
+//disable eslint in this line
+debugger; //eslint-disable-line no-debugger
 
 const courseValue = numeral(1000).format('$0,0.00');
 console.log(`I would pay ${courseValue}  for this awesome course !`);
---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+import {expect} from 'chai';
+import jsdom from 'jsdom'
+
+describe('Our first test',() => {
+  it('should pass',() => {
+    expect(true).to.equal(true);
+  });
+});

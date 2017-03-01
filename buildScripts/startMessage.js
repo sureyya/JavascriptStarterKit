af5b732cfc41550a2152685996c8d5c43e42f5b1
Uslu, Sureyya <Sureyya.Uslu@ca.com>
2017-03-01 22:30:32 +0100

    Add addtional npm scripts, add run parallel, add babel support

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,8 @@
-var chalk = require('chalk');
+
+//ES5 common.js style
+//var chalk = require('chalk');
+
+//ES6
+import chalk from 'chalk';
 
 console.log(chalk.green('Starting app in dev mode...'));
